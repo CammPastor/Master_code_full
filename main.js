d0431628a88019c2a3ef3a0c6a1a92073ae04134
CammPastor <campastor.acosta@gmail.com>
2022-05-18 00:12:50 -0300

    subiendo repositorio

---
+++ main.js
@@ -0,0 +1,8 @@
+const inicio = document.querySelector (".inicio")
+const nosotros = document.querySelector (".nosotros")
+const form = document.querySelector (".formulario")
+const main = document.querySelector (".container")
+nosotros.addEventListener("click", () => {
+        main.innerHTML= "<h1>hola</h1>"
+    
+    })
